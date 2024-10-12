55910d5cee9cbb22abba48036c6d834d0550332f
MuhRandy <muhammadrandy0027@gmail.com>
2024-10-12 08:31:18 +0800

    chore: change index.js to main.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const LinkedList = require("./models/LinkedList");
-const util = require("util");
-
-const linkedList = new LinkedList();
---
+++ main.js
@@ -0,0 +1,13 @@
+const LinkedList = require("./models/LinkedList");
+const util = require("util");
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
